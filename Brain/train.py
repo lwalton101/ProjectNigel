a2cac277d523deeda42a6992240eee63f6ff696c
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-05-05 20:03:31 +0100

    updated to new train method -tf tutorial

--- Brain/train.py
+++ Brain/train.py
@@ -1,120 +1,114 @@
+# %%
+import matplotlib.pyplot as plt
 import os
-os.environ["TF_USE_LEGACY_KERAS"] ="1"
-
-from intent.train import RawData, JointIntentAndSlotFillingModel, read_train_json_file
-from intent.train.slot import encode_slots
+import re
+import shutil
+import string
 import tensorflow as tf
-from transformers import AutoTokenizer
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
-from tensorflow.keras.metrics import SparseCategoricalAccuracy
-from intent.train.encode import encode_intents, encode_texts
 
-train_data: list[RawData] = read_train_json_file("./intent/train/train.json")
+from tensorflow import keras
+from keras import layers,losses
+batch_size = 32
+seed = 42
 
-model_name = "bert-base-cased"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+    'intent/train/dataset/',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed)
 
-texts = [d.text for d in train_data]
-tds = encode_texts(tokenizer, texts)
-encoded_texts = tds
+i = 0
+for label_name in raw_train_ds.class_names:
+    print(f"Label {i} is {label_name}")
+    i += 1
 
-intents = [d.intent for d in train_data]
-intent_names = list(set(intents))
+raw_val_ds = tf.keras.utils.text_dataset_from_directory(
+    'intent/train/dataset/',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed)
 
-intent_map = dict() # index -> intent
-for idx, ui in enumerate(intent_names):
-    intent_map[ui] = idx
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+  return tf.strings.regex_replace(stripped_html,
+                                  '[%s]' % re.escape(string.punctuation),
+                                  '')
 
-encoded_intents = encode_intents(intents, intent_map)
+max_features = 10000
+sequence_length = 250
 
-slot_names = set()
-for td in train_data:
-    slots = td.slots
-    for slot in slots:
-        slot_names.add(slot)
-slot_names = list(slot_names)
-slot_names.insert(0, "<PAD>")
+vectorize_layer = layers.TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
 
-slot_map = dict() # slot -> index
-for idx, us in enumerate(slot_names):
-    slot_map[us] = idx
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
 
-max_len = len(encoded_texts["input_ids"][0])
-all_slots = [td.slots for td in train_data]
-all_texts = [td.text for td in train_data]
-encoded_slots = encode_slots(all_slots, all_texts, tokenizer, slot_map, max_len)
+def vectorize_text(text, label):
+  text = tf.expand_dims(text, -1)
+  return vectorize_layer(text), label
 
-joint_model = JointIntentAndSlotFillingModel(model_name,
-    intent_num_labels=len(intent_map), slot_num_labels=len(slot_map))
+text_batch, label_batch = next(iter(raw_train_ds))
+first_review, first_label = text_batch[0], label_batch[0]
+print("Review", first_review)
+print("Label", raw_train_ds.class_names[first_label])
+print("Vectorized review", vectorize_text(first_review, first_label))
 
-opt = Adam(learning_rate=3e-5, epsilon=1e-08)
-losses = [SparseCategoricalCrossentropy(from_logits=True),
-          SparseCategoricalCrossentropy(from_logits=True)]
-metrics = [SparseCategoricalAccuracy("accuracy")]
-# compile model
-joint_model.compile(optimizer=opt, loss=losses, metrics=metrics)
+print("1287 ---> ",vectorize_layer.get_vocabulary()[1287])
+print(" 313 ---> ",vectorize_layer.get_vocabulary()[313])
+print('Vocabulary size: {}'.format(len(vectorize_layer.get_vocabulary())))
 
-x = {"input_ids": encoded_texts["input_ids"], "token_type_ids": encoded_texts["token_type_ids"],  "attention_mask": encoded_texts["attention_mask"]}
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
 
-history = joint_model.fit(
-    x, (encoded_slots, encoded_intents), epochs=1, batch_size=32, shuffle=True)
+AUTOTUNE = tf.data.AUTOTUNE
 
-if not os.path.exists("./intent/models"):
-    os.mkdir("./intent/models")
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-joint_model.save("./intent/models/model1")
+embedding_dim = 16
+model = tf.keras.Sequential([
+  layers.Embedding(max_features, embedding_dim),
+  layers.Dropout(0.2),
+  layers.GlobalAveragePooling1D(),
+  layers.Dropout(0.2),
+  layers.Dense(7, activation='sigmoid')])
 
-def nlu(text, tokenizer, model, intent_names, slot_names):
-    inputs = tf.constant(tokenizer.encode(text))[None, :]  # batch_size = 1
-    outputs = model(inputs)
-    slot_logits, intent_logits = outputs
+model.summary()
 
-    slot_ids = slot_logits.numpy().argmax(axis=-1)[0, :]
-    intent_id = intent_logits.numpy().argmax(axis=-1)[0]
+model.compile(loss=losses.SparseCategoricalCrossentropy(),
+              optimizer='adam',
+              metrics=['accuracy'])
 
-    info = {"intent": intent_names[intent_id], "slots": {}}
+epochs = 10
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs)
 
-    out_dict = {}
-    # get all slot names and add to out_dict as keys
-    predicted_slots = set([slot_names[s] for s in slot_ids if s != 0])
-    for ps in predicted_slots:
-      out_dict[ps] = []
+export_model = tf.keras.Sequential([
+  vectorize_layer,
+  model,
+  layers.Activation('sigmoid')
+])
 
-    # check if the text starts with a small letter
-    if text[0].islower():
-      tokens = tokenizer.tokenize(text, add_special_tokens=True)
-    else:
-      tokens = tokenizer.tokenize(text)
-    for token, slot_id in zip(tokens, slot_ids):
-        # add all to out_dict
-        slot_name = slot_names[slot_id]
+export_model.compile(
+    loss=losses.SparseCategoricalCrossentropy(from_logits=False), optimizer="adam", metrics=['accuracy']
+)
 
-        if slot_name == "<PAD>":
-            continue
+examples = tf.constant([
+  "Play Bad Blood by Taylor Swift",
+  "Get the weather in Southern Ohio",
+  "Can you add a song to my bops playlist"
+])
 
-        # collect tokens
-        collected_tokens = [token]
-        idx = tokens.index(token)
+predictions = export_model.predict(examples)
+j = [raw_train_ds.class_names[prediction.argmax()] for prediction in predictions]
+export_model.export("./intent/train/model")
 
-        # see if it starts with ##
-        # then it belongs to the previous token
-        if token.startswith("##"):
-          # check if the token already exists or not
-          if tokens[idx - 1] not in out_dict[slot_name]:
-            collected_tokens.insert(0, tokens[idx - 1])
 
-        # add collected tokens to slots
-        out_dict[slot_name].extend(collected_tokens)
-
-    # process out_dict
-    for slot_name in out_dict:
-        tokens = out_dict[slot_name]
-        slot_value = tokenizer.convert_tokens_to_string(tokens)
-
-        info["slots"][slot_name] = slot_value.strip()
-
-    return info
-
-print(nlu("add Madchild to Electro Latino", tokenizer, joint_model, 
-    intent_names, slot_names))

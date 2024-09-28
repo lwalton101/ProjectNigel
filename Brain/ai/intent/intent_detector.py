7d63ad7a1b6b873d04375febdffffced1afc0f63
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-09-28 11:49:46 +0100

    detect intents

--- Brain/ai/intent/intent_detector.py
+++ Brain/ai/intent/intent_detector.py
@@ -1,15 +1,16 @@
+import os
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+import tensorflow as tf
 class IntentDetector:
     def __init__(self, model_name: str) -> None:
-        raise Exception("Not implemented yet")
-        pass
+        self.load_model(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
         
     def load_model(self, model_name: str) -> None:
-        raise Exception("Not implemented yet")
+        self.model = TFAutoModelForSequenceClassification.from_pretrained(f"./Brain/ai/intent/models/{model_name}")
+        pass
         
-    def detect_intent(self, text_input: str) -> str:
-        raise Exception("Not implemented yet")
-    
-    def detect_intents(self, text_inputs: str) -> str:
-        raise Exception("Not implemented yet")
-        
-        
+    def detect_intent(self, text_input: str):
+        inputs = self.tokenizer(text_input, return_tensors="tf")
+        outputs = self.model(**inputs).logits
+        return outputs

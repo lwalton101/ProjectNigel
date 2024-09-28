3a8995ceb63f4f31a5cf925363b9836282145a91
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-09-28 14:16:42 +0100

    detect speed tests

--- Brain/examples/fill_slots_speed.py
+++ Brain/examples/fill_slots_speed.py
@@ -1,12 +1,14 @@
 from ..ai.intent import IntentDetector
 import datetime
-id = IntentDetector("0.1")
+from ..ai.slot import SlotFiller
+
+id = SlotFiller("Nicknotname/Project_Nigel_Intent_Detection")
 startTime = datetime.datetime.now()
 
 tries = 100
 print("Starting Test")
 for x in range(tries):
-    id.detect_intent("Play Hello by adele")
+    id.fill_slots("Play hello by adele")
     
 print(f"Total Tries: {tries}")
 print(f"Total Time: {datetime.datetime.now() - startTime}")
